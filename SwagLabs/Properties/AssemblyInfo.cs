2c0177def549f240857e7c9b52153c2fa0beebb4
AarishWorks <aarishirfan@gmail.com>
2023-03-30 15:05:24 +0500

    Initial Commit by Arish

---
+++ SwagLabs/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SwagLabs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SwagLabs")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a24177a3-a1e8-418d-9057-0f090dddf921")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SwagLabs/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace SwagLabs
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [TestCategory("log4Net")]
+        public void TestMethod001()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.saucedemo.com/";
+            driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+            driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+            driver.FindElement(By.Id("login-button")).Click();
+            string actualText = driver.FindElement(By.ClassName("app_logo")).Text;
+            Assert.AreEqual("Swag Labs", actualText);
+        }
+        [TestMethod]
+        [TestCategory("log4Net")]
+        public void TestMethod002()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.saucedemo.com/";
+            driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+            driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+            driver.FindElement(By.Id("login-button")).Click();
+            var select = driver.FindElement(By.ClassName("product_sort_container"));
+            var selectDropDown = new SelectElement(select);
+            selectDropDown.SelectByValue("lohi");
+            driver.FindElement(By.Id("add-to-cart-sauce-labs-onesie")).Click();
+            driver.FindElement(By.ClassName("shopping_cart_link")).Click();
+            driver.FindElement(By.Id("checkout")).Click();
+            driver.FindElement(By.Id("first-name")).SendKeys("Aarish");
+            driver.FindElement(By.Id("last-name")).SendKeys("Irfan");
+            driver.FindElement(By.Id("postal-code")).SendKeys("75280");
+            driver.FindElement(By.Id("continue")).Click();
+            driver.FindElement(By.Id("finish")).Click();
+            string actualText = driver.FindElement(By.ClassName("complete-header")).Text;
+            Assert.AreEqual("Thank you for your order!", actualText);
+            driver.FindElement(By.Id("back-to-products")).Click();
+            driver.Close();
+        }
+    }
+}
